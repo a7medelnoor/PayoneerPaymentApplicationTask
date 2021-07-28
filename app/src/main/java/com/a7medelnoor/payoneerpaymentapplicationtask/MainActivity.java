19d4c6d6525056b155e2b1d106ced4330010144e
Ahmed Elnoor <acc.ahmedelnoor.adam@gmail.com>
2021-07-28 12:46:04 +0200

    Initial commit

---
+++ app/src/main/java/com/a7medelnoor/payoneerpaymentapplicationtask/MainActivity.java
@@ -0,0 +1,15 @@
+package com.a7medelnoor.payoneerpaymentapplicationtask;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
