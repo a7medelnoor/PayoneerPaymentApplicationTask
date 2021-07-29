6330a2d85a19d8f51dcbd0fd9bcec4d4cbd6b39e
Ahmed Elnoor <acc.ahmedelnoor.adam@gmail.com>
2021-07-29 14:30:49 +0200

    create application for whole the project

---
+++ app/src/main/java/com/a7medelnoor/payoneerpaymentapplicationtask/MyApplication.java
@@ -0,0 +1,9 @@
+package com.a7medelnoor.payoneerpaymentapplicationtask;
+
+import android.app.Application;
+
+import dagger.hilt.android.HiltAndroidApp;
+
+@HiltAndroidApp
+public class MyApplication extends Application {
+}
