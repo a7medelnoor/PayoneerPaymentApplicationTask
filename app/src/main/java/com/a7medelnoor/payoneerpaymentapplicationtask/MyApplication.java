17cd819a2a22f31f4649f0ef27d81882c14f42cb
Ahmed Elnoor <acc.ahmedelnoor.adam@gmail.com>
2021-08-02 15:34:14 +0200

    base application class

--- app/src/main/java/com/a7medelnoor/payoneerpaymentapplicationtask/MyApplication.java
+++ app/src/main/java/com/a7medelnoor/payoneerpaymentapplicationtask/MyApplication.java
@@ -2,8 +2,6 @@ package com.a7medelnoor.payoneerpaymentapplicationtask;
 
 import android.app.Application;
 
-import dagger.hilt.android.HiltAndroidApp;
 
-@HiltAndroidApp
 public class MyApplication extends Application {
 }
