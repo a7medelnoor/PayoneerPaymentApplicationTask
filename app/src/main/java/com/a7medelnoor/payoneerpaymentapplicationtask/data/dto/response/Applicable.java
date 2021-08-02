d3975eea11c0b8529b10174abc4859b34e65792d
Ahmed Elnoor <acc.ahmedelnoor.adam@gmail.com>
2021-08-02 15:47:29 +0200

    response class Applicable

---
+++ app/src/main/java/com/a7medelnoor/payoneerpaymentapplicationtask/data/dto/response/Applicable.java
@@ -0,0 +1,24 @@
+package com.a7medelnoor.payoneerpaymentapplicationtask.data.dto.response;
+
+public class Applicable {
+    public String label;
+    public Links links;
+
+
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    public Links getLinks() {
+        return links;
+    }
+
+    public void setLinks(Links links) {
+        this.links = links;
+    }
+}
