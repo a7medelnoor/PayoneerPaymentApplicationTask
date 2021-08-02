7a855cefcbcfc0126edf9dc49c2b26681ba8c54d
Ahmed Elnoor <acc.ahmedelnoor.adam@gmail.com>
2021-08-02 15:43:52 +0200

    create response links class

---
+++ app/src/main/java/com/a7medelnoor/payoneerpaymentapplicationtask/data/dto/response/Links.java
@@ -0,0 +1,17 @@
+package com.a7medelnoor.payoneerpaymentapplicationtask.data.dto.response;
+
+public class Links {
+    public String logo;
+
+    public String getLogo() {
+        return logo;
+    }
+
+    public Links(String logo) {
+        this.logo = logo;
+    }
+
+    public void setLogo(String logo) {
+        this.logo = logo;
+    }
+}
