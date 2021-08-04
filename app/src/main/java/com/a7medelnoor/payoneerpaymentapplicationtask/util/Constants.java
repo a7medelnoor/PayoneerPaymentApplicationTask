4ab80d306e95f779a264039e78d47cd4c85370d3
Ahmed Elnoor <acc.ahmedelnoor.adam@gmail.com>
2021-08-04 12:13:17 +0200

    change the base url content

--- app/src/main/java/com/a7medelnoor/payoneerpaymentapplicationtask/util/Constants.java
+++ app/src/main/java/com/a7medelnoor/payoneerpaymentapplicationtask/util/Constants.java
@@ -1,7 +1,7 @@
 package com.a7medelnoor.payoneerpaymentapplicationtask.util;
 
 public class Constants {
-    public static final String BASE_URL = "https://raw.githubusercontent.com/";
+    public static final String BASE_URL = "https://raw.githubusercontent.com";
 
     public static final String PAYMENT_METHOD_LOGO = "https://raw.githubusercontent.com/optile/checkout-android/develop/checkout/src/main/assets/networklogos/";
 }
