353ca7ac1e0f2128337ecf35bb257a27a61885d9
Ahmed Elnoor <acc.ahmedelnoor.adam@gmail.com>
2021-08-04 16:08:54 +0200

    getter and setter

--- app/src/main/java/com/a7medelnoor/payoneerpaymentapplicationtask/data/dto/response/Applicable.java
+++ app/src/main/java/com/a7medelnoor/payoneerpaymentapplicationtask/data/dto/response/Applicable.java
@@ -1,8 +1,10 @@
 package com.a7medelnoor.payoneerpaymentapplicationtask.data.dto.response;
-
 public class Applicable {
     public String label;
     public Links links;
+
+    public Applicable() {
+    }
 
     public String getLabel() {
         return label;
@@ -12,4 +14,11 @@ public class Applicable {
         return links;
     }
 
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    public void setLinks(Links links) {
+        this.links = links;
+    }
 }
