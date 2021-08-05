067d1162464bb89c6a4b8c383274e6e3f966746c
Ahmed Elnoor <acc.ahmedelnoor.adam@gmail.com>
2021-08-05 13:51:46 +0200

    add class info for our duffCallback and itemCallback

--- app/src/main/java/com/a7medelnoor/payoneerpaymentapplicationtask/util/MyDiffCallback.java
+++ app/src/main/java/com/a7medelnoor/payoneerpaymentapplicationtask/util/MyDiffCallback.java
@@ -5,7 +5,21 @@ import androidx.recyclerview.widget.DiffUtil;
 import com.a7medelnoor.payoneerpaymentapplicationtask.data.dto.response.Applicable;
 
 import java.util.List;
+/**
+ * Created by Ahmed Elnoor
+ */
 
+/**
+ * All information contained herein is, and remains the property of Payoneer.
+ * Unauthorized use, duplication, reverse engineering, any form of redistribution,
+ * or use in part or in whole requires prior, express, printed and signed permission from Payoneer.
+ * <p>
+ * MyDiffCallback class for our list items
+ *
+ * @author Ahmed Elnoor
+ * @version 0.1, 27-07-2021
+ * @authorAccount https://github.com/a7medelnoor
+ */
 public class MyDiffCallback extends DiffUtil.Callback {
 
     List<Applicable> oldItem;
--- app/src/main/java/com/a7medelnoor/payoneerpaymentapplicationtask/util/MyItemCallback.java
+++ app/src/main/java/com/a7medelnoor/payoneerpaymentapplicationtask/util/MyItemCallback.java
@@ -6,7 +6,21 @@ import androidx.recyclerview.widget.DiffUtil;
 import com.a7medelnoor.payoneerpaymentapplicationtask.data.dto.response.Applicable;
 
 import org.jetbrains.annotations.NotNull;
+/**
+ * Created by Ahmed Elnoor
+ */
 
+/**
+ * All information contained herein is, and remains the property of Payoneer.
+ * Unauthorized use, duplication, reverse engineering, any form of redistribution,
+ * or use in part or in whole requires prior, express, printed and signed permission from Payoneer.
+ * <p>
+ * MyItemCallback class for our list items
+ *
+ * @author Ahmed Elnoor
+ * @version 0.1, 27-07-2021
+ * @authorAccount https://github.com/a7medelnoor
+ */
 public class MyItemCallback  extends DiffUtil.ItemCallback {
     Applicable applicable;
     @Override
