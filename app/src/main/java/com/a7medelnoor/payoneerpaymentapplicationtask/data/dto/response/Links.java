1d519b8194a5b878952fd30c6295e007af5777e6
Ahmed Elnoor <acc.ahmedelnoor.adam@gmail.com>
2021-08-05 13:48:49 +0200

    add class info and add to string method for our links model class

--- app/src/main/java/com/a7medelnoor/payoneerpaymentapplicationtask/data/dto/response/Links.java
+++ app/src/main/java/com/a7medelnoor/payoneerpaymentapplicationtask/data/dto/response/Links.java
@@ -1,14 +1,26 @@
 package com.a7medelnoor.payoneerpaymentapplicationtask.data.dto.response;
+/**
+ * Created by Ahmed Elnoor
+ */
 
-public class Links {
+/**
+ * All information contained herein is, and remains the property of Payoneer.
+ * Unauthorized use, duplication, reverse engineering, any form of redistribution,
+ * or use in part or in whole requires prior, express, printed and signed permission from Payoneer.
+ *
+ * Links Model Class
+ *
+ * @author Ahmed Elnoor
+ * @version 0.1, 05-08-2021
+ * @authorAccount https://github.com/a7medelnoor
+ */
+public class Links{
+    @Override
+    public String toString() {
+        return "Links{" +
+                "logo='" + logo + '\'' +
+                '}';
+    }
+
     public String logo;
-
-    public String getLogo() {
-        return logo;
-    }
-
-    public void setLogo(String logo) {
-        this.logo = logo;
-    }
-
 }
