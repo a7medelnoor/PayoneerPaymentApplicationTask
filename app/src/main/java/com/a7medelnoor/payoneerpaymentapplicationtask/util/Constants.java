ba92fc208d9ae3c0f43d7f7bce71fcb07220ed9a
Ahmed Elnoor <acc.ahmedelnoor.adam@gmail.com>
2021-08-05 13:45:04 +0200

    add class information and configure

--- app/src/main/java/com/a7medelnoor/payoneerpaymentapplicationtask/util/Constants.java
+++ app/src/main/java/com/a7medelnoor/payoneerpaymentapplicationtask/util/Constants.java
@@ -1,7 +1,19 @@
 package com.a7medelnoor.payoneerpaymentapplicationtask.util;
+/**
+ * Created by Ahmed Elnoor
+ */
 
+/**
+ * All information contained herein is, and remains the property of Payoneer.
+ * Unauthorized use, duplication, reverse engineering, any form of redistribution,
+ * or use in part or in whole requires prior, express, printed and signed permission from Payoneer.
+ * <p>
+ * Constants class that holds all our constants
+ *
+ * @author Ahmed Elnoor
+ * @version 0.1, 27-07-2021
+ * @authorAccount https://github.com/a7medelnoor
+ */
 public class Constants {
-    public static final String BASE_URL = "https://raw.githubusercontent.com/optile/checkout-android/develop/shared-test/";
-
-    public static final String PAYMENT_METHOD_LOGO = "https://raw.githubusercontent.com/optile/checkout-android/develop/checkout/src/main/assets/networklogos/";
+    public static final String BASE_URL = "https://raw.githubusercontent.com/";
 }
