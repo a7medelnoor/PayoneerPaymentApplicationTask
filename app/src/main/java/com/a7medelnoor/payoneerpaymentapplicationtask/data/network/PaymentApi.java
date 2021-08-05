8b1c684811a6ea22769509bb055bb1e34bc64a46
Ahmed Elnoor <acc.ahmedelnoor.adam@gmail.com>
2021-08-05 13:55:00 +0200

    add class info and change our base response to be the BaseResponse class, also change our get url

--- app/src/main/java/com/a7medelnoor/payoneerpaymentapplicationtask/data/network/PaymentApi.java
+++ app/src/main/java/com/a7medelnoor/payoneerpaymentapplicationtask/data/network/PaymentApi.java
@@ -1,16 +1,27 @@
 package com.a7medelnoor.payoneerpaymentapplicationtask.data.network;
 
-import com.a7medelnoor.payoneerpaymentapplicationtask.data.dto.response.JSONResponse;
-import com.google.gson.JsonObject;
-
-import org.json.JSONObject;
+import com.a7medelnoor.payoneerpaymentapplicationtask.data.dto.response.BaseResponse;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+/**
+ * Created by Ahmed Elnoor
+ */
 
+/**
+ * All information contained herein is, and remains the property of Payoneer.
+ * Unauthorized use, duplication, reverse engineering, any form of redistribution,
+ * or use in part or in whole requires prior, express, printed and signed permission from Payoneer.
+ * <p>
+ * PaymentApi Interface for our api response
+ *
+ * @author Ahmed Elnoor
+ * @version 0.1, 27-07-2021
+ * @authorAccount https://github.com/a7medelnoor
+ */
 public interface PaymentApi {
 
-    @GET("lists/listresult.json")
-    Call<JsonObject> getAllMethodPayment();
+    @GET("optile/checkout-android/develop/shared-test/lists/listresult.json")
+    Call<BaseResponse> getAllMethodPayment();
 
 }
