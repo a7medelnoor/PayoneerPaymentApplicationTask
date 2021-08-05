3f8c9add8d0aebdb66bdbd72fdb9aeabc82144f7
Ahmed Elnoor <acc.ahmedelnoor.adam@gmail.com>
2021-08-05 13:54:00 +0200

    add class info and refactor our request to be requested form our Repository

--- app/src/main/java/com/a7medelnoor/payoneerpaymentapplicationtask/viewModel/MainActivityViewModel.java
+++ app/src/main/java/com/a7medelnoor/payoneerpaymentapplicationtask/viewModel/MainActivityViewModel.java
@@ -1,81 +1,46 @@
 package com.a7medelnoor.payoneerpaymentapplicationtask.viewModel;
 
-import android.app.Application;
-import android.util.Log;
-
-import androidx.annotation.NonNull;
-import androidx.lifecycle.AndroidViewModel;
-import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.ViewModel;
 
 import com.a7medelnoor.payoneerpaymentapplicationtask.data.Repository;
 import com.a7medelnoor.payoneerpaymentapplicationtask.data.dto.response.Applicable;
-import com.a7medelnoor.payoneerpaymentapplicationtask.data.dto.response.JSONResponse;
-import com.a7medelnoor.payoneerpaymentapplicationtask.data.network.remote.ApiClient;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-
-import org.json.JSONArray;
-import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.List;
+/**
+ * Created by Ahmed Elnoor
+ */
 
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
+/**
+ * All information contained herein is, and remains the property of Payoneer.
+ * Unauthorized use, duplication, reverse engineering, any form of redistribution,
+ * or use in part or in whole requires prior, express, printed and signed permission from Payoneer.
+ * <p>
+ * MainActivityViewModel class for making request and observe our list to ui via liveData
+ *
+ * @author Ahmed Elnoor
+ * @version 0.1, 27-07-2021
+ * @authorAccount https://github.com/a7medelnoor
+ */
+public class MainActivityViewModel extends ViewModel {
+    List<Applicable> arraylist;
+    LiveData<List<Applicable>> listLiveData;
+    private final Repository repository = new Repository().getInstance();
 
-public class MainActivityViewModel extends AndroidViewModel {
-    Repository repository;
-    public MutableLiveData<List<Applicable>> listMutableLiveData = new MutableLiveData<>();
-    MutableLiveData<Boolean> loading = new MutableLiveData<>(false);
-    private static final String TAG = "MainActivityViewModel";
-    Application application;
-    ArrayList<String> label = new ArrayList<>();
-    ArrayList<String> linksArraylist = new ArrayList<>();
-    List<Applicable> applicables;
-    JSONResponse jsonResponse;
-    ArrayList<Applicable> data;
-
-    public MainActivityViewModel(@NonNull Application application) {
-        super(application);
-        this.application = application;
-//        getPaymentMethod();
+    public MainActivityViewModel() {
+        super();
+        arraylist = new ArrayList<>();
+        listLiveData = repository.getApplicable();
     }
 
-//    public void getPaymentMethod() {
-//        applicables = new ArrayList<>();
-//        loading.setValue(true);
-//        ApiClient.getPaymentService(application).getAllMethodPayment().enqueue(new Callback<JsonObject>() {
-//            @Override
-//            public void onResponse(Call<JsonObject> call, Response<JsonObject> response) {
-//                JsonArray applicable = response.body().getAsJsonObject("networks").getAsJsonArray("applicable");
-//                for (int i = 0; i < applicable.size(); i++){
-//                    Log.d(TAG, "onResponse: fsbssgsdgsgsrgsrgsrg"+applicable);
-//                    applicables.addAll(applicables);
-//                    listMutableLiveData.setValue(applicables);
-//                }
-//                Log.d(TAG, "onResponse: WWWWWWWWWWWWWWWWWWW"+applicable);
-//                Log.d(TAG, "onResponse: MainViewModel "+response.body().getAsJsonObject("networks"));
-//                JsonObject jsonObject = response.body().getAsJsonObject("networks");
-//                JsonArray array = jsonObject.getAsJsonArray("applicable");
-////                ArrayList<Applicable> applicable = new ArrayList<>();
-//////                applicable.add(applicable.get());
-////                Log.d(TAG, "onResponse: ffffffffff"+array);
-////                for (int i=0; i < array.size(); i++){
-////                    Applicable arrayDetails = array.get(i);
-////                    Log.d(TAG, "onResponse: dssssssssssssssss"+arrayDetails.getLabel());
-////                    label.add(arrayDetails.getAsString());
-//////                    listMutableLiveData.setValue();
-////                }
-//
-//            }
-//
-//            @Override
-//            public void onFailure(Call<JsonObject> call, Throwable throwable) {
-//
-//            }
-//        });
-//
-//    }
+    @Override
+    protected void onCleared() {
+        super.onCleared();
+    }
+
+    public LiveData<List<Applicable>> getListLiveData() {
+        return listLiveData;
+    }
+
 }
