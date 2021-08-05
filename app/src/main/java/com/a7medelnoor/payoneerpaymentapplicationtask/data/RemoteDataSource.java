3aa0927056bf7aaf1476db746170141bbd386acd
Ahmed Elnoor <acc.ahmedelnoor.adam@gmail.com>
2021-08-05 14:03:05 +0200

    add class info implements our api interface

--- app/src/main/java/com/a7medelnoor/payoneerpaymentapplicationtask/data/RemoteDataSource.java
+++ app/src/main/java/com/a7medelnoor/payoneerpaymentapplicationtask/data/RemoteDataSource.java
@@ -1,18 +1,30 @@
 package com.a7medelnoor.payoneerpaymentapplicationtask.data;
 
-import com.a7medelnoor.payoneerpaymentapplicationtask.data.dto.response.JSONResponse;
+import com.a7medelnoor.payoneerpaymentapplicationtask.data.dto.response.BaseResponse;
 import com.a7medelnoor.payoneerpaymentapplicationtask.data.network.PaymentApi;
-import com.google.gson.JsonObject;
-
-import org.json.JSONObject;
+import com.a7medelnoor.payoneerpaymentapplicationtask.data.network.remote.ApiClient;
 
 import retrofit2.Call;
+/**
+ * Created by Ahmed Elnoor
+ */
 
-public class RemoteDataSource implements PaymentApi {
+/**
+ * All information contained herein is, and remains the property of Payoneer.
+ * Unauthorized use, duplication, reverse engineering, any form of redistribution,
+ * or use in part or in whole requires prior, express, printed and signed permission from Payoneer.
+ * <p>
+ * RemoteDataSource class for our remote api response
+ *
+ * @author Ahmed Elnoor
+ * @version 0.1, 27-07-2021
+ * @authorAccount https://github.com/a7medelnoor
+ */
+public class RemoteDataSource implements PaymentApi{
     PaymentApi paymentApi;
 
     @Override
-    public Call<JsonObject> getAllMethodPayment() {
+    public Call<BaseResponse> getAllMethodPayment() {
         return paymentApi.getAllMethodPayment();
     }
 }
