63364a36569bf7124ead6b9a367f79bf67481cb3
Ahmed Elnoor <acc.ahmedelnoor.adam@gmail.com>
2021-08-05 13:42:02 +0200

    add class information and add to string function

--- app/src/main/java/com/a7medelnoor/payoneerpaymentapplicationtask/data/dto/response/Applicable.java
+++ app/src/main/java/com/a7medelnoor/payoneerpaymentapplicationtask/data/dto/response/Applicable.java
@@ -1,24 +1,32 @@
 package com.a7medelnoor.payoneerpaymentapplicationtask.data.dto.response;
+/**
+ * Created by Ahmed Elnoor
+ */
+
+/**
+ * All information contained herein is, and remains the property of Payoneer.
+ * Unauthorized use, duplication, reverse engineering, any form of redistribution,
+ * or use in part or in whole requires prior, express, printed and signed permission from Payoneer.
+ *
+ * Applicable Model Class
+ *
+ * @author Ahmed Elnoor
+ * @version 0.1, 05-08-2021
+ * @authorAccount https://github.com/a7medelnoor
+ */
 public class Applicable {
     public String label;
     public Links links;
-
-    public Applicable() {
-    }
-
-    public String getLabel() {
-        return label;
-    }
 
     public Links getLinks() {
         return links;
     }
 
-    public void setLabel(String label) {
-        this.label = label;
-    }
-
-    public void setLinks(Links links) {
-        this.links = links;
+    @Override
+    public String toString() {
+        return "Applicable{" +
+                "label='" + label + '\'' +
+                ", links=" + links +
+                '}';
     }
 }
